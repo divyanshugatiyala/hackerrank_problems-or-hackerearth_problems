93557755d741bfb8d39c8e122637398c962755f9
BhumikaJena <bhumikajena7609@gmail.com>
2022-09-28 23:04:47 +0530

    added the code

--- JAVA/STraversingAMatrix.java
+++ JAVA/STraversingAMatrix.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class SpirallyTraversingMatrix {
+public class STraversingAMatrix {
 
 	public static void main(String[] args) {
 		int matrix[][] = {{9, 2, 0, 4},
